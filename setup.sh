22ed65695a6805eff563951381c9745b54806996
kjm4027 <gameliker16@naver.com>
2024-12-02 15:26:35 +0900

    setup파일 추가, sdkman, java, gradle 설치 후 build

---
+++ setup.sh
@@ -0,0 +1,14 @@
+ 1727  curl -s "https://get.sdkman.io" | bash
+ 1728  ls
+ 1729  source "/home/kjm4027/.sdkman/bin/sdkman-init.sh"
+ 1730  curl -s "https://get.sdkman.io" | bash
+ 1731  sudo apt install zip
+ 1732  curl -s "https://get.sdkman.io" | bash
+ 1733  source "/home/kjm4027/.sdkman/bin/sdkman-init.sh"
+ 1734  sdk install java 17.0.2-open
+ 1735  sdk default java 17.0.2-open
+ 1736  java -version
+ 1737  sdk install gradle
+ 1738  gradle -v
+ 1739  ./gradlew build
+ 1740  history
